74e7b6543f1d2e636e209837422ae03423f30c32
Sindre Korsnes <55540102+sindkor@users.noreply.github.com>
2023-12-18 16:15:20 +0100

    Import appx using Windows Powershell

--- scripts/Install-MsixPackagingTool.ps1
+++ scripts/Install-MsixPackagingTool.ps1
@@ -27,7 +27,7 @@ Invoke-WebRequest $MSIXPackagingToolInstallationFileUrl -OutFile $TempDirectoryP
 Invoke-WebRequest $MSIXPackagingToolLicenseFileUrl -OutFile $TempDirectoryPath\MSIXPackagingToolLicense.xml
 
 Add-AppxProvisionedPackage -Online -PackagePath $TempDirectoryPath\MSIXPackagingTool.msixbundle -LicensePath $TempDirectoryPath\MSIXPackagingToolLicense.xml
-Import-Module Appx -Verbose
+Import-Module Appx -UseWindowsPowerShell
 $package = Get-AppxPackage -Name "Microsoft.MSIXPackagingTool"
 Write-Output "PackageFamilyName is $($package.PackageFamilyName)"
 $initalizeMsixPackagingToolCmd = "explorer.exe shell:AppsFolder\$($package.PackageFamilyName)!Msix.app"
